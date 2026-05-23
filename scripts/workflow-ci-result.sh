3e4c9e8a532ea238829ad49ba9b7bc73abd0d5de
Kashif <kashif.dev@protonmail.com>
2026-05-23 12:37:35 +0530

    refactor(ci): raise a PR updating CHANGELOG

--- scripts/workflow-ci-result.sh
+++ scripts/workflow-ci-result.sh
@@ -16,5 +16,6 @@ check_result() {
 
 check_result "changes" "${1:?changes result is required}"
 check_result "rust" "${2:?rust result is required}"
-check_result "ts" "${3:?ts result is required}"
-check_result "security-audit" "${4:?security-audit result is required}"
+check_result "scripts" "${3:?scripts result is required}"
+check_result "ts" "${4:?ts result is required}"
+check_result "security-audit" "${5:?security-audit result is required}"
